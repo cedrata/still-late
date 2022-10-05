0c8c5e2a9287c672da9f92366f2d03ca4421a893
cedrata <81557163+cedrata@users.noreply.github.com>
2022-10-05 23:02:21 +0200

    [ADD] delay line files

---
+++ Source/cdrt/DelayLine.cpp
@@ -0,0 +1,8 @@
+//
+//  DelayLine.cpp
+//  StillLate
+//
+//  Created by Cedrata on 05/10/22.
+//
+
+#include "DelayLine.hpp"
---
+++ Source/cdrt/DelayLine.hpp
@@ -0,0 +1,10 @@
+//
+//  DelayLine.hpp
+//  StillLate
+//
+//  Created by Cedrata on 05/10/22.
+//
+
+#pragma once
+
+#include <stdio.h>
